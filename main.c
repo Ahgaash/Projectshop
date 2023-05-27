68061ca207c33e8b272a26fd07d0313da5187a89
Ahgaash <133210716+Ahgaash@users.noreply.github.com>
2023-05-27 19:32:11 +0200

    main.c

--- main.c
+++ main.c
@@ -6,10 +6,10 @@ int main(){
    int choix;   // Variable pour stocker le choix de l'utilisateur
    int num_products;
    Product products[MAX_PRODUCTS]; // Tableau pour stocker les produits
-  printf("Welcome to ATM.INC 🚗\n"); 
-  printf("Unleash the Power of Elegance and Performance.\n");
+  printf("Welcome to ATM.INC 🚗💨\n"); 
+  printf("~ Unleash the Power of Elegance ✨ and Performance ⚡ ~\n");
   do{
-  printf("Choice 1 for 'purchase mode' or 2 for 'management mode': ");
+  printf("Choose 1 for 'purchase mode 🛒' or 2 for 'management mode ⚙️ ': ");
     scanf("%d",&choix);
     empty_buffer();
   } while (choix!=1 && choix!=2);
@@ -23,5 +23,3 @@ int main(){
     
   }
      
-    
-
